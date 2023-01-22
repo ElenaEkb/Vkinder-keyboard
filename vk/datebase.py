08a72a7b81426ecbb0162f5fb44d5a22c6efe23e
LenaLebedeva <moonlikee@mail.ru>
2023-01-22 20:51:56 +0500

    база данных

--- vk/datebase.py
+++ vk/datebase.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 import psycopg2
 
-def creat_db():
+def create_db():
     initial_connection = psycopd2.connect(
         database='diplom',
         user='postgres',
@@ -11,4 +11,22 @@ def creat_db():
     )
     initial_connection.close()
 
+def create_tables():
+    connection.execute('''
+    CREATE TABLE IF NOT EXISTS friend_users (
+	id integer PRIMARY KEY,
+	first_name varchar(40) NOT NULL UNIQUE,
+	last_name varchar(40) NOT NULL UNIQUE,
+	photo1 integer NOT NULL,
+	photo2 integer NOT NULL,
+	photo3 integer NOT NULL,
+    );
+    ''')
+
+def insert_users(user, photo_data):
+    if not connection.execute(f" SELECT id FROM friend_users WHERE id={user['id']}").fetchone():
+        connection.execute(f"INSERT INTO friend_users (id, first_name, last_name, photo1, photo2, photo3)"
+                           f"VALUES ({user['id']},\'{user['first_name']}\',"
+                           f"\'{user['last_name']}\', {photo_data['photo_ids'][0]},"
+                           f"{photo_data['photo_ids'][1]}, {photo_data['photo_ids'][2]});")
 
